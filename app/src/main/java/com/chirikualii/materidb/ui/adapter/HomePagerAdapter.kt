525f6df657e2ad80227bd059433b29d3d0443ee1
Roisull <lusiro2001@students.amikom.ac.id>
2022-12-21 00:28:58 +0700

    implement linter in project binar academy challenge chapter 8

--- app/src/main/java/com/chirikualii/materidb/ui/adapter/HomePagerAdapter.kt
+++ app/src/main/java/com/chirikualii/materidb/ui/adapter/HomePagerAdapter.kt
@@ -2,13 +2,7 @@ package com.chirikualii.materidb.ui.adapter
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentPagerAdapter
-import androidx.fragment.app.FragmentStateManagerControl
-import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.chirikualii.materidb.ui.home.nowPlaying.NowPlayingFragment
-import com.chirikualii.materidb.ui.home.popularMovie.PopularMovieFragment
 
 class HomePagerAdapter(fragmentActivity: FragmentActivity, val listFragment :List<Fragment>,val listTitle:List<String>) : FragmentStateAdapter(fragmentActivity) {
 
